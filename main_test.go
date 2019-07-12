08b60c561ce6b92c5932f4f540e5a3544b71a10a
Mark Billie <mbillie1@gmail.com>
2019-07-12 15:42:14 -0600

    reduce tests

--- main_test.go
+++ main_test.go
@@ -7,13 +7,23 @@ func TestReduceInts(t *testing.T) {
 		f   func(int, int) int
 		arr []int
 	}
+
+	add := func(x, y int) int { return x + y }
+	multiply := func(x, y int) int { return x * y }
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
 	tests := []struct {
 		name string
 		args args
 		want int
 	}{
-		{"reduce + [1,2,3] is 6", args{func(x, y int) int { return x + y }, []int{1, 2, 3}}, 6},
-		{"reduce * [2,3,4] is 24", args{func(x, y int) int { return x * y }, []int{2, 3, 4}}, 24},
+		{"reduce add [1,2,3] is 6", args{add, []int{1, 2, 3}}, 6},
+		{"reduce multiply [2,3,4] is 24", args{multiply, []int{2, 3, 4}}, 24},
+		{"reduce max [4,1,9,4,11,4,11,3 is 11", args{max, []int{4, 1, 9, 4, 11, 4, 11, 3}}, 11},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
