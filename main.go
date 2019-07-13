6d5373c79e48ba1f7ab4c12fe802d79f60e8df87
Mark Billie <mbillie1@gmail.com>
2019-07-13 12:47:42 -0600

    this is clearer I think

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
-func reduceInt(f func(int, int) int, arr []int) int {
+type reduceFunc func(int, int) int
+type filterFunc func(int) bool
+
+func reduceInt(f reduceFunc, arr []int) int {
 	var next, result int
 	result = arr[0]
 	for i := 1; i < len(arr); i++ {
@@ -12,7 +15,7 @@ func reduceInt(f func(int, int) int, arr []int) int {
 	return result
 }
 
-func filterInt(f func(int) bool, arr []int) []int {
+func filterInt(f filterFunc, arr []int) []int {
 	results := make([]int, 0)
 	for _, i := range arr {
 		if f(i) {
